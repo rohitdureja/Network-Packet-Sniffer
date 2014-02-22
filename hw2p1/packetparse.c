d043da171f697f37fa6f260dd0582a43a5554697
Bolei Fu <bfu@plus.seas.upenn.edu>
2014-02-22 18:07:12 -0500

    print packet

--- hw2p1/packetparse.c
+++ hw2p1/packetparse.c
@@ -1,36 +1,31 @@
 #include<stdio.h>
 #include"pcap.h"
 
-void dispatcher
+
+void packet_handler(u_char* user, const struct pcap_pkthdr *pkt_header, const u_char * pkt_data){
+	int i;
+	printf("P:");
+	for(i=1;i<(pkt_header->caplen+1);i++){
+	    printf("%.2x",pkt_data[i-1]);
+		printf(" ");
+	}
+	printf("\n\n");
+}
+
 
 int main(int argc, char *argv[])
 {
-	
-	char *dev, errbuf[PCAP_ERRBUF_SIZE];
+	char errbuf[PCAP_ERRBUF_SIZE];
 	pcap_t *handle;	
-	/*Set up device*/
-	if(argc<2)
-	{
-		dev = pcap_lookupdev(errbuf);
-		if(dev==NULL)
-		{	
-			fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-			return(2);
-		}
-		printf("Device: %s\n", dev);
+
+	handle = pcap_open_offline("sampleimf.pcap",errbuf);
+	
+	if(handle==NULL){
+	    fprintf(stderr,"%s",errbuf);
 	}
-	else
-	 {
-		dev = argv[1];
-		printf("Device: %s\n", dev);
-	}
-	/*Open device for sniffing*/
-	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-	if(handle==NULL)
-	{
-		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-		return(2);
-	}
+	
+	pcap_loop(handle,0,packet_handler,NULL);
+	
 	
 	return 0;
 }
